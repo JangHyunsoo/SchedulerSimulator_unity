1abde461665bacc7ab82cfad1be3e23fd278dba9
JangHyunsoo <hunsoo911@gmail.com>
2023-05-06 11:14:29 +0900

    Add : MultiProcess script add MultiProcess script

--- Assets/Script/Data/MultiProcess.cs
+++ Assets/Script/Data/MultiProcess.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 
 public class MultiProcess : Process
 {
-    
-
     public MultiProcess(Job _job) : base(_job) { }
 
-    public void completeProcess()
-    {
-        completed_process_count_++;
-        if (completed_process_count_ == divided_process_count_) need_merge_ = true;
-    }
 }
